4d2807509e2a0b690a48fe30520ba86d2901cf5c
naka-c1024 <ny42800824@gmail.com>
2022-03-20 17:21:05 +0900

    add: ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (size_t i = 1; argv[i]; i++)
+		{
+			for (size_t j = 0; argv[i][j]; j++)
+				argv[i][j] = toupper(argv[i][j]);
+			std::cout << argv[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
