8811fae6da2f10d0c5db28e45c6b9cbc879cefe3
naka-c1024 <ny42800824@gmail.com>
2022-08-01 22:14:41 +0900

    ex02 done

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,6 +1,8 @@
 #include "Account.hpp"
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include <sstream>
 
 // 静的メンバ変数は、インスタンスを生成しなくても呼び出すことができる。
 // 静的メンバ変数の宣言はhppで、初期化はソースファイルで行われる。
@@ -13,59 +15,142 @@ int	Account::_totalNbWithdrawals = 0;
 
 int	Account::getNbAccounts( void )
 {
-
+	return (Account::_nbAccounts); // 静的メンバ変数だからthisは使えない
 }
 int	Account::getTotalAmount( void )
 {
-
+	return (Account::_totalAmount);
 }
 int	Account::getNbDeposits( void )
 {
-
+	return (Account::_totalNbDeposits);
 }
 int	Account::getNbWithdrawals( void )
 {
-
+	return (Account::_totalNbWithdrawals);
 }
 void	Account::displayAccountsInfos( void )
 {
-
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts()
+		<< ";total:" << getTotalAmount()
+		<< ";deposits:" << getNbDeposits()
+		<< ";withdrawals:" << getNbWithdrawals()
+		<< std::endl;
 }
 
 Account::Account( int initial_deposit )
 {
+	_displayTimestamp();
+	std::cout << "index:" << getNbAccounts()
+		<< ";amount:" << initial_deposit
+		<< ";created" << std::endl;
 
+	this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+}
+Account::Account( void )
+{
+	_displayTimestamp();
+	std::cout << "index:" << getNbAccounts()
+		<< ";amount:" << "0"
+		<< ";created" << std::endl;
+
+	this->_accountIndex = _nbAccounts;
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	_nbAccounts++;
 }
 Account::~Account( void )
 {
+	_nbAccounts--;
+	_totalAmount -= this->_amount;
+	_totalNbDeposits -= this->_nbDeposits;
+	_totalNbWithdrawals -= this->_nbWithdrawals;
 
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";closed" << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+		<< ";p_amount:" << this->_amount
+		<< ";deposit:" << deposit << ";";
 
+	this->_amount += deposit;
+	(this->_nbDeposits)++;
+	_totalNbDeposits ++;
+	_totalAmount += deposit;
+
+	std::cout << "amount:" << this->_amount
+		<< ";nb_deposits:" << this->_nbDeposits << std::endl;
 }
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	int	p_amount = this->_amount;
+	this->_amount -= withdrawal;
 
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+		<< ";p_amount:" << p_amount
+		<< ";withdrawal:";
+	if (checkAmount())
+	{
+		(this->_nbWithdrawals)++;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals ++;
+		std::cout << withdrawal
+			<< ";amount:" << this->_amount
+			<< ";nb_withdrawals:" << this->_nbWithdrawals
+			<< std::endl;
+		return (true);
+	}
+	else
+	{
+		this->_amount = p_amount;
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
 }
 int		Account::checkAmount( void ) const
 {
-
+	return (this->_amount >= 0);
 }
 void	Account::displayStatus( void ) const
 {
-
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";deposits:" << this->_nbDeposits
+		<< ";withdrawals:" << this->_nbWithdrawals
+		<< std::endl;
 }
 
 // private func
 
 void	Account::_displayTimestamp( void )
 {
+	time_t t = time(nullptr);
+	const tm* localTime = localtime(&t);
 
-}
-
-Account::Account( void )
-{
-
+	std::cout << "[";
+	std::cout << localTime->tm_year + 1900;
+	std::cout << std::setw(2) << std::setfill('0') << localTime->tm_mon + 1;
+	std::cout << std::setw(2) << std::setfill('0') << localTime->tm_mday;
+	std::cout << "_";
+	std::cout << std::setw(2) << std::setfill('0') << localTime->tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << localTime->tm_min;
+	std::cout << std::setw(2) << std::setfill('0') << localTime->tm_sec;
+	std::cout << "] ";
 }
--- ex02/tests.cpp
+++ ex02/tests.cpp
@@ -15,53 +15,53 @@
 
 int		main( void ) {
 
-	typedef std::vector<Account::t>							  accounts_t;
+	typedef std::vector<Account::t>							  accounts_t; // ここでAccountをaccounts_tに置き換え
 	typedef std::vector<int>								  ints_t;
 	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
 
-	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
+	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 }; // initial_deposit
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts( amounts, amounts + amounts_size );
+	accounts_t				accounts( amounts, amounts + amounts_size ); // インスタンスの作成 -> コンストラクタの起動
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
-	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
+	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 }; // deposits預金額
 	size_t const		d_size( sizeof(d) / sizeof(int) );
 	ints_t				deposits( d, d + d_size );
 	ints_t::iterator	dep_begin	= deposits.begin();
 	ints_t::iterator	dep_end		= deposits.end();
 
-	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
+	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 }; // withdrawals引き出し額
 	size_t const		w_size( sizeof(w) / sizeof(int) );
 	ints_t				withdrawals( w, w + w_size );
 	ints_t::iterator	wit_begin	= withdrawals.begin();
 	ints_t::iterator	wit_end		= withdrawals.end();
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	Account::displayAccountsInfos(); // 全体の情報を出力, 静的メンバ関数
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) ); // 詳細情報の出力
 
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
 		  ++(it.first), ++(it.second) ) {
 
-		(*(it.first)).makeDeposit( *(it.second) );
+		(*(it.first)).makeDeposit( *(it.second) ); // deposits記録の出力
 	}
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	Account::displayAccountsInfos(); // 全体の情報を出力
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) ); // 詳細情報の出力
 
 	for ( acc_int_t it( acc_begin, wit_begin );
 		  it.first != acc_end && it.second != wit_end;
 		  ++(it.first), ++(it.second) ) {
 
-		(*(it.first)).makeWithdrawal( *(it.second) );
+		(*(it.first)).makeWithdrawal( *(it.second) ); // withdrawal記録の出力
 	}
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	Account::displayAccountsInfos(); // 全体の情報を出力
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) ); // 詳細情報の出力
 
 	return 0;
-}
+} // デストラクタの出力
 
 
 // ************************************************************************** //
